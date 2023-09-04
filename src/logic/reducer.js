07990a2790d22cfc617de7306ba729fb0034c3db
Matt30Curia <matteocu10@hotmail.com>
2023-09-04 13:14:52 +0200

    Update reducer.js

--- src/logic/reducer.js
+++ src/logic/reducer.js
@@ -2,13 +2,14 @@ export function reducer(state, action) {
   let currYear = state.year;
   let currMonth = state.month;
   let currDay = state.day;
-  let lastDayInMonth = new Date(currDate.year, currDate.month, 0).getDay();
+
 
   switch (action.type) {
     case "nextMonth": {
-      const [year, month, day] = new Date(currYear, currMonth , currDay + 1).toISOString().split("-");
+      currYear = state.month === 12 ? state.year + 1 : state.year;
+      currMonth = state.month === 12 ? 1 : state.month + 1;
 
-      return { day: day.split('T')[0], month: month, year};
+      return { day: state.day, month: currMonth, year: currYear };
     }
     case "prevMonth": {
       currYear = state.month === 1 ? state.year - 1 : state.year;
@@ -16,16 +17,6 @@ export function reducer(state, action) {
 
       return { day: state.day, month: currMonth, year: currYear };
     }
-    case "nextDay": {
-      currDay = lastDayInMonth === currDay + 1
-
-      return { day: state.day, month: currMonth, year: currYear };
-    }
-    case "prevDay": {
-      currYear = state.month === 1 ? state.year - 1 : state.year;
-      currMonth = state.month === 1 ? 12 : state.month - 1;
-
-      return { day: state.day, month: currMonth, year: currYear };
-    }
+   
   }
 }
