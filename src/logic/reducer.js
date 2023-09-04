b587d7a4c7fbaeda894022c3041ea985732b8d46
Matt30Curia <matteocu10@hotmail.com>
2023-09-04 12:08:45 +0200

    Update reducer.js

--- src/logic/reducer.js
+++ src/logic/reducer.js
@@ -2,54 +2,18 @@ export function reducer(state, action) {
   let currYear = state.year;
   let currMonth = state.month;
   let currDay = state.day;
-  let lastDayInMonth = new Date(currDate.year, currDate.month, 0).getDay();
 
   switch (action.type) {
     case "nextMonth": {
-      currYear = state.month === 12 ? state.year + 1 : state.year;
-      currMonth = state.month === 12 ? 1 : state.month + 1;
+      const [year, month, day] = new Date(currYear, currMonth , currDay + 1).toISOString().split("-");
 
-      return { day: state.day, month: currMonth, year: currYear };
+      return { day: day.split('T')[0], month: month, year};
     }
     case "prevMonth": {
-      currYear = state.month === 1 ? state.year - 1 : state.year;
-      currMonth = state.month === 1 ? 12 : state.month - 1;
 
-      return { day: state.day, month: currMonth, year: currYear };
-    }
-    case "nextDay": {
-      currDay = lastDayInMonth === currDay + 1
+      const [year, month, day] = new Date(currYear, currMonth - 2 , currDay + 1).toISOString().split("-");
 
-      return { day: state.day, month: currMonth, year: currYear };
-    }
-    case "prevDay": {
-      currYear = state.month === 1 ? state.year - 1 : state.year;
-      currMonth = state.month === 1 ? 12 : state.month - 1;
-
-      return { day: state.day, month: currMonth, year: currYear };
+      return { day: day.split('T')[0], month: month, year};
     }
   }
-}
-
-function nextDay(days, date){
-
-  let {day, month, year} = date;
-
-  let lastDayInMonth = new Date(currDate.year, currDate.month, 0).getDay();
-  
-  if (days === lastDayInMonth){
-    let {nextMonth, nextYear} = nextMonth(month, year)
-    day = 1; month = nextMonth; year = nextYear;
-  }
-
-  else{
-    day++;
-  }
-
-  function nextMonth(month, year) {
-    let currYear = month === 12 ? year + 1 : year;
-    let currMonth = month === 12 ? 1 : month + 1;
-    return { currMonth, currYear };
-  }
-  
 }
